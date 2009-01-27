9570f6644e6890e6369845cc962f6a2d6ac06a35
Benjamin Smedberg <benjamin@smedbergs.us>
2009-01-26 22:26:22 -0500

    The rules of makefile parsing require that you expand things you meet before you tokenize? Or only partially?

--- tests/dynamic-var.mk
+++ tests/dynamic-var.mk
@@ -5,10 +5,13 @@ VARNAME = FOOBAR
 $(VARNAME) = foovalue
 $(VARNAME)2 = foo2value
 
+$(VARNAME:%BAR=%BAM) = foobam
+
 all:
 	test "$(FOOBAR)" = "foovalue"
 	test "$(flavor FOOBAZ)" = "undefined"
 	test "$(FOOBAR2)" = "bazvalue"
+	test "$(FOOBAM)" = "foobam"
 	@echo TEST-PASS
 
 VARNAME = FOOBAZ
