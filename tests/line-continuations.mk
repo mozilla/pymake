723e5371e7ff15b1466c2ae55afc7551e4648d08
Benjamin Smedberg <benjamin@smedbergs.us>
2009-01-30 15:28:33 -0500

    Just think, I wasting brain space with makefile line continuations!

--- tests/line-continuations.mk
+++ tests/line-continuations.mk
@@ -1,8 +1,12 @@
 VAR = val1 	 \
   	  val2  
 
+VAR2 = val1space\
+val2
+
 all:
 	test "$(VAR)" = "val1 val2  "
+	test "$(VAR2)" = "val1space val2"
 	test "hello \
 	  world" = "hello   world"
 	test "hello" = \
