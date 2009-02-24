df758cb38ec71e794a0caef462a38d30b62d43c5
Benjamin Smedberg <benjamin@smedbergs.us>
2009-02-24 09:57:24 -0500

    WaitForMultipleObjects had a race condition on opening a process handle via PID on a process that was already defunct. Use minor knowledge of subprocess internals (subp._handle) to re-use the existing handle instead of trying to find a safe way of creating a new one.

--- pymake/process.py
+++ pymake/process.py
@@ -6,9 +6,7 @@ parsing command lines into argv and making sure that no shell magic is being use
 import subprocess, shlex, re, logging, sys, traceback, os
 import command, util
 if sys.platform=='win32':
-    import ctypes
-    SYNCHRONIZE = 0x00100000
-    INFINITE = -1
+    import win32process
 
 _log = logging.getLogger('pymake.process')
 
@@ -79,10 +77,6 @@ class ParallelContext(object):
 
     _allcontexts = set()
 
-    # For Windows, we need to keep track of process handles
-    # so we can wait on them.
-    _handles = {}
-
     def __init__(self, jcount):
         self.jcount = jcount
         self.exit = False
@@ -108,7 +102,7 @@ class ParallelContext(object):
     def _docall(self, argv, shell, env, cwd, cb, echo):
             if echo is not None:
                 print echo
-            p = ParallelContext._popen(argv, shell=shell, env=env, cwd=cwd)
+            p = subprocess.Popen(argv, shell=shell, env=env, cwd=cwd)
             self.running.append((p, cb))
 
     def call(self, argv, shell, env, cwd, cb, echo):
@@ -118,31 +112,23 @@ class ParallelContext(object):
 
         self.defer(self._docall, argv, shell, env, cwd, cb, echo)
 
-    @staticmethod
-    def _popen(argv, **kwargs):
-        p = subprocess.Popen(argv, **kwargs)
-        if sys.platform=='win32':
-            # we need a handle so we can wait on it
-            h = ctypes.windll.kernel32.OpenProcess(SYNCHRONIZE, False, p.pid)
-            ParallelContext._handles[h] = p.pid
-        return p
+    if sys.platform == 'win32':
+	@staticmethod
+	def _waitany():
+            return win32process.WaitForAnyProcess([p for c in ParallelContext._allcontexts for p, cb in c.running])
 
-    @staticmethod
-    def _waitanypid():
-        if sys.platform != 'win32':
-            pid, status = os.waitpid(-1, 0)
-        else:
-            arrtype = ctypes.c_long * len(ParallelContext._handles)
-            handle_array = arrtype(*ParallelContext._handles.keys())
-            ret = ctypes.windll.kernel32.WaitForMultipleObjects(len(handle_array), handle_array, False, INFINITE)
-            h = handle_array[ret]
-            exitcode = ctypes.c_long()
-            ctypes.windll.kernel32.GetExitCodeProcess(h, ctypes.byref(exitcode))
-            ctypes.windll.kernel32.CloseHandle(h)
-            pid = ParallelContext._handles[h]
-            status = exitcode.value <<8
-            del ParallelContext._handles[h]
-        return (pid, status)
+        @staticmethod
+        def _comparepid(pid, process):
+            return pid == process
+
+    else:
+        @staticmethod
+        def _waitany():
+            return os.waitpid(-1, 0)
+
+        @staticmethod
+        def _comparepid(pid, process):
+            return pid == process.pid
 
     @staticmethod
     def spin():
@@ -160,14 +146,14 @@ class ParallelContext(object):
             dowait = any((len(c.running) for c in ParallelContext._allcontexts))
 
             if dowait:
-                pid, status = ParallelContext._waitanypid()
+                pid, status = ParallelContext._waitany()
                 result = statustoresult(status)
 
                 found = False
                 for c in ParallelContext._allcontexts:
                     for i in xrange(0, len(c.running)):
                         p, cb = c.running[i]
-                        if p.pid == pid:
+                        if ParallelContext._comparepid(pid, p):
                             del c.running[i]
                             cb(result)
                             found = True
